fb6e9a7ecc1e00fc2f00b829e22c5c2ada122050
Rahul <avengers2016.lma@.com>
2016-07-21 23:02:48 +0530

    Code changes for new code

---
+++ jersey-services/src/main/java/com/jersey/dao/UserDao.java
@@ -0,0 +1,78 @@
+/**
+ * 
+ */
+package com.jersey.dao;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.jersey.model.User;
+/**
+ * @author RAHUL
+ *
+ */
+public class UserDao {
+
+	public List<User> getAllUsers(){
+		List<User> userList = new ArrayList<User>();
+
+		User user1 = new User(1, "Rahul", "Developer");
+		
+		userList.add(user1);
+		return userList;
+	}
+	
+	public User getUser(int id){
+		List<User> users = getAllUsers();
+		for(User user: users){
+			if(user.getId() == id){
+				return user;
+			}
+		}
+		return null;
+	}
+	
+	public int addUser(User pUser){
+		List<User> userList = getAllUsers();
+		boolean userExists = false;
+		for(User user: userList){
+			if(user.getId() == pUser.getId()){
+				userExists = true;
+				break;
+			}
+		}
+		if(!userExists){
+			userList.add(pUser);
+
+			return 1;
+		}
+		return 0;
+	}
+	
+	public int updateUser(User pUser){
+		List<User> userList = getAllUsers();
+		for(User user: userList){
+			if(user.getId() == pUser.getId()){
+				int index = userList.indexOf(user);
+				userList.set(index, pUser);
+				return 1;
+			}
+		}
+		return 0;
+	}
+	
+	public int deleteUser(int id){
+		List<User> userList = getAllUsers();
+		for(User user: userList){
+			if(user.getId() == id){
+				int index = userList.indexOf(user);
+				userList.remove(index);
+				return 1;
+			}
+		}
+		return 0;
+	}
+	
+	
+	
+}
---
+++ jersey-services/src/main/java/com/jersey/resource/UserService.java
@@ -0,0 +1,87 @@
+/**
+ * 
+ */
+package com.jersey.resource;
+
+import java.io.IOException;
+import java.util.List;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.FormParam;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import com.jersey.dao.UserDao;
+import com.jersey.model.User;
+
+/**
+ * @author RAHUL
+ *
+ */
+@Path("/UserService")
+public class UserService {
+
+	UserDao userDao = new UserDao();
+	private static final String SUCCESS_RESULT="<result>success</result>";
+	private static final String FAILURE_RESULT="<result>failure</result>";
+
+	// http://localhost:8080/jersey-services/webapi/UserService/users
+	@GET
+	@Path("/users")
+	@Produces({MediaType.APPLICATION_XML})
+	public List<User> getUsers(){
+		return userDao.getAllUsers();
+	}
+
+	@GET
+	@Path("/users/{userid}")
+	@Produces(MediaType.APPLICATION_XML)
+	public User getUser(@PathParam ("userid") int userid){
+		return userDao.getUser(userid);
+	}
+
+	@PUT
+	@Path("/users")
+	@Produces(MediaType.APPLICATION_XML)
+	@Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+	public String createUser(@FormParam ("id") int id,@FormParam ("name") String name,@FormParam ("profession") String profession) throws IOException{
+		User user = new User(id, name, profession);
+		int result = userDao.addUser(user);
+		if(result == 1){
+			return SUCCESS_RESULT;
+		}
+		return FAILURE_RESULT;
+	}
+
+	@POST
+	@Path("/users")
+	@Produces(MediaType.APPLICATION_XML)
+	@Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+	public String updateUser(@FormParam ("id") int id,
+			@FormParam ("name") String name,
+			@FormParam ("profession") String profession) throws IOException{
+		User user = new User(id, name, profession);
+		int result = userDao.updateUser(user);
+		if(result == 1){
+			return SUCCESS_RESULT;
+		}
+		return FAILURE_RESULT;
+	}
+
+	@DELETE
+	@Path("/users/{userid}")
+	@Produces(MediaType.APPLICATION_XML)
+	public String deleteUser(@PathParam ("userid") int userid){
+		int result = userDao.deleteUser(userid);
+		if(result == 1){
+			return SUCCESS_RESULT;
+		}
+		return FAILURE_RESULT;
+	}
+}
